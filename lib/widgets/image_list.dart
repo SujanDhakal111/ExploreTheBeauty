e7436c5dd5ef80c937f97bb42b8cd26ea442289f
SujanDhakal111 <sdhakal361@gmail.com>
2020-04-08 09:42:30 +0545

    Final layout

--- lib/widgets/image_list.dart
+++ lib/widgets/image_list.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../models/image_model.dart';
 
@@ -12,10 +13,29 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Image.network(images[index].url);
-
+       return buildImage(images[index]);
       },
     );
+  }
+  Widget buildImage(ImageModel image) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      padding: EdgeInsets.all(20.0),
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            child:  Image.network(image.url),
+            padding: EdgeInsets.only(bottom:8.0),
+          ),
+
+          Text(image.title),
+        ],
+      ),
+    );
+
   }
 
 
