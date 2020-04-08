9fafa704824562e0623c519a708689f51730dda8
SujanDhakal111 <sdhakal361@gmail.com>
2020-04-08 08:56:49 +0545

    displaying images from url

--- lib/widgets/image_list.dart
+++ lib/widgets/image_list.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import '../models/image_model.dart';
 
-class ImageList extends StatelessWidget{
+//stateless widgets contain data that shouldot channge over time
+class ImageList extends StatelessWidget {
 
-  List<ImageModel> images;
-  ImageList(this.images)
+  //while using ststeless widget always set variables as final
+  final List<ImageModel> images;
+  ImageList(this.images);
+
+  Widget build(context) {
+    return ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (context, int index) {
+        return Image.network(images[index].url);
+
+      },
+    );
+  }
+
+
 }
